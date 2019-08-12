73ad0b14bf86c0c670a279dd54b1db0db392a716
Emerson F Couto <emerfcouto@hotmail.com>
2019-08-11 21:44:53 -0300

    Salvando os dados do usuario logado no localStorage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -35,7 +35,7 @@ export class HomePage {
 
     this.auth.authenticate(this.creds)
       .subscribe(response => {
-        console.log(response.headers.get('Authorization'));
+        this.auth.successfulLogin(response.headers.get('Authorization'));
         this.navCtrl.setRoot('CategoriasPage');
       },
       error => {});
