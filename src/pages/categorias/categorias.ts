7df40fb7a5a1d846c7e24218856c17023bd1316b
Emerson F Couto <emerfcouto@hotmail.com>
2019-08-12 21:50:55 -0300

    Criando uma pagina de profile

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -4,13 +4,6 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 import { CategoriaDTO } from '../../models/categoria.dto';
 import { API_CONFIG } from '../../config/api.config';
 
-/**
- * Generated class for the CategoriasPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-categorias',
