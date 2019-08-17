0f48debd08485c1159e24bee583c1f96dfc12a7b
Emerson F Couto <emerfcouto@hotmail.com>
2019-08-17 16:27:01 -0300

    Carregando produtos de uma dada categoria

---
+++ src/services/domain/produto.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from "@angular/common/http";
+import { API_CONFIG } from "../../config/api.config";
+import { Injectable } from "@angular/core";
+
+
+@Injectable()
+export class ProdutoService {
+
+    constructor(public http: HttpClient) {
+    }
+
+    findByCategoria(categoria_id : string) {
+
+        return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+    }
+}
