9241d32140178fcd1d1d3f2df1a866da289d78d7
Emerson F Couto <emerfcouto@hotmail.com>
2019-08-20 21:02:32 -0300

    Refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -22,6 +22,11 @@ export class ProdutosPage {
 
   ionViewDidLoad() {
 
+    this.loadData();
+  }
+
+  loadData() {
+
     let categoria_id = this.navParams.get('categoria_id');
 
     let loader = this.presentLoading();
@@ -62,5 +67,12 @@ export class ProdutosPage {
     loader.present();
     
     return loader;
+  }
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
   }
 }
