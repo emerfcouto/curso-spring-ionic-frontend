6e21ed4d76ec27d05456747379a9dd69d3d1e9b1
Emerson F Couto <emerfcouto@hotmail.com>
2019-08-23 19:12:10 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8081",
+    baseUrl: "https://curso-spring-ionic-emerfcouto.herokuapp.com",
     bucketBaseUrl: "https://curso-spring-ionic-emerfcouto.s3-sa-east-1.amazonaws.com"
 }
